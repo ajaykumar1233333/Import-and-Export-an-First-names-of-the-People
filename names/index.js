b382ac29e7dc1ccf5c3a69c075b1167ec5f4fe76
Ajay Kumar K <kinneraajay7@gmail.com>
2023-07-14 05:53:42 +0000

    Import and Export First names of people

---
+++ names/index.js
@@ -0,0 +1,8 @@
+const a = require("../country/state/city/index");
+const b = require("../utilities/utils/index");
+
+const getPeopleInCity = (c) => {
+  return b(c);
+};
+
+module.exports = getPeopleInCity;
